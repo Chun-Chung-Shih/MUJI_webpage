bc1f03d0a055b2059cd86b8fbb3f065454aa9000
Chun-Chung-Shih <sharon8839@gmail.com>
2022-03-07 21:57:12 +0800

    upload edited HTML and SCSS file

--- muji/js/main.js
+++
@@ -1,9 +0,0 @@
-//All your JS code goes here
-// Open the full screen search box
-function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
-  }
-  
-  function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-  }
