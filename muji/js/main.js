045961eef2c4339100c7140dc3324110f91dbfab
Chun-Chung-Shih <sharon8839@gmail.com>
2022-03-07 21:46:38 +0800

    upload muji folder

---
+++ muji/js/main.js
@@ -0,0 +1,9 @@
+//All your JS code goes here
+// Open the full screen search box
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
